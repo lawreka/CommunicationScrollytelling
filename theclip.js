fd05e8bb47a6ac4cdebac69c7804f6881616a0fb
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2017-07-01 18:07:46 +0200

    first push let's see if it works online

---
+++ theclip.js
@@ -0,0 +1,24 @@
+//this is how wapoints work
+var waypoint = new Waypoint({
+  element: $("#title"),
+  handler: function(){
+    console.log("waypoint");
+  }
+});
+
+//play an animation
+// agent.Play('Searching');
+//play a random animation
+// agent.animate();
+//get a list of all the animations
+// agent.animations();
+//show text balloon
+// agent.speak('Blah');
+//move to the given point, use animation if available
+// agent.moveTo(100, 100);
+//gesture at a given point (if gesture animation is available)
+// agent.gestureAt(200,200);
+//stop the current animation in the queue
+// agent.stopCurrent();
+//stop all actions in the queue and go back to idle mode
+// agent.stop();
