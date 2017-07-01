0fd6bf2597ccc30802181cee5d095d2fc21a6b07
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2017-07-01 20:56:02 +0200

    getting there - carousel implemented, start laying out first written sections

--- theclip.js
+++ theclip.js
@@ -1,3 +1,22 @@
+setInterval(function () {
+        moveRight();
+    }, 3000);
+var slideCount = $('#slider ul li').length;
+var slideWidth = $('#slider ul li').width();
+var slideHeight = $('#slider ul li').height();
+var sliderUlWidth = slideCount * slideWidth;
+$('#slider').css({ width: slideWidth, height: slideHeight });
+$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+$('#slider ul li:last-child').prependTo('#slider ul');
+function moveRight() {
+  $('#slider ul').animate({
+      left: - slideWidth
+      }, 200, function () {
+         $('#slider ul li:first-child').appendTo('#slider ul');
+         $('#slider ul').css('left', '');
+       });
+};
+
 //this is how wapoints work
 var waypoint = new Waypoint({
   element: $("#title"),
