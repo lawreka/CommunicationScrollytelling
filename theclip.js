aa0fe6338913745bad315c465a1c0608dfc2175b
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2017-07-06 17:10:17 +0200

    finished Clippy JS + sources!

--- theclip.js
+++ theclip.js
@@ -27,27 +27,29 @@ var waypoint = new Waypoint({
           agent.play('LookLeft');
           agent.play('Wave');
           agent.speak("That's me!");
+          agent.play('IdleSideToSide');
           agent.play("GestureDown");
       });
+    }
+    else{
+     $(".clippy").remove();
     }
   }
 });
 var waypoint = new Waypoint({
   element: $("#text2"),
-  handler: function(direction){
-    if (direction == "down"){
-      $(".clippy").remove();
-      $(".clippy-balloon").remove();
-    }
+  handler: function(){
+     $(".clippy").remove();
+     $(".clippy-balloon").remove();
   }
 });
 var waypoint = new Waypoint({
-  element: $("#text3"),
+  element: $("#section2"),
   handler: function(direction){
     if (direction == "down"){
       clippy.load('Clippy', function(agent) {
-          agent.moveTo(50, 100);
-          agent.show();
+        agent.moveTo(50, 100);
+        agent.show();
           agent.speak("Mi casa es su CASA!");
       });
     }
@@ -57,15 +59,307 @@ var waypoint = new Waypoint({
   }
 });
 var waypoint = new Waypoint({
+  element: $("#rover"),
+  handler: function(){
+     $(".clippy").remove();
+     $(".clippy-balloon").remove();
+  }
+});
+var waypoint = new Waypoint({
   element: $("#patents"),
   handler: function(direction){
     if (direction == "down"){
-      $(".clippy").remove();
-      $(".clippy-balloon").remove();
+      clippy.load('Clippy', function(agent) {
+        agent.moveTo(50, 100);
+        agent.show();
+          agent.play('LookDownLeft');
+          agent.speak("Lookin' good, since 1997!");
+      });
+    }
+    else{
+     $(".clippy").remove();
     }
   }
 });
-
+var waypoint = new Waypoint({
+  element: $("#nass"),
+  handler: function(){
+     $(".clippy").remove();
+     $(".clippy-balloon").remove();
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#g4vid"),
+  handler: function(direction){
+    if (direction == "down"){
+      clippy.load('Clippy', function(agent) {
+        agent.moveTo(50, 100);
+        agent.show();
+          agent.play('Hearing_1');
+      });
+    }
+    else{
+     $(".clippy").remove();
+    }
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#vidquote"),
+  handler: function(){
+     $(".clippy").remove();
+     $(".clippy-balloon").remove();
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#characters"),
+  handler: function(direction){
+    if (direction == "down"){
+      clippy.load('Clippy', function(agent) {
+        agent.moveTo(50, 100);
+        agent.show();
+          agent.speak("Whatever you want me to be");
+          agent.play("GetArtsy");
+          agent.play("GetTechy");
+      });
+    }
+    else{
+     $(".clippy").remove();
+    }
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#wrong"),
+  handler: function(){
+     $(".clippy").remove();
+     $(".clippy-balloon").remove();
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#section3"),
+  handler: function(direction){
+    if (direction == "down"){
+      clippy.load('Clippy', function(agent) {
+        agent.moveTo(50, 100);
+        agent.show();
+          agent.play("Writing");
+          agent.play("Searching");
+          agent.play("EmptyTrash");
+          agent.play("Save");
+      });
+    }
+    else{
+     $(".clippy").remove();
+    }
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#joke"),
+  handler: function(){
+     $(".clippy").remove();
+     $(".clippy-balloon").remove();
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#gender"),
+  handler: function(direction){
+    if (direction == "down"){
+      clippy.load('Clippy', function(agent) {
+        agent.moveTo(50, 100);
+        agent.show();
+          agent.speak("Ladies love me");
+      });
+    }
+    else{
+     $(".clippy").remove();
+    }
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#genderquote"),
+  handler: function(){
+     $(".clippy").remove();
+     $(".clippy-balloon").remove();
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#kevan"),
+  handler: function(direction){
+    if (direction == "down"){
+      clippy.load('Clippy', function(agent) {
+        agent.moveTo(50, 100);
+        agent.show();
+        agent.play("GestureLeft");
+          agent.speak("That's my dad!");
+      });
+    }
+    else{
+     $(".clippy").remove();
+    }
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#kevquote"),
+  handler: function(){
+     $(".clippy").remove();
+     $(".clippy-balloon").remove();
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#polarized"),
+  handler: function(direction){
+    if (direction == "down"){
+      clippy.load('Clippy', function(agent) {
+        agent.moveTo(50, 100);
+        agent.show();
+          agent.speak("Totally harsh, dude");
+      });
+    }
+    else{
+     $(".clippy").remove();
+    }
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#worst"),
+  handler: function(){
+     $(".clippy").remove();
+     $(".clippy-balloon").remove();
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#kindchen"),
+  handler: function(direction){
+    if (direction == "down"){
+      clippy.load('Clippy', function(agent) {
+        agent.moveTo(50, 100);
+        agent.show();
+          agent.play("GetAttention");
+      });
+    }
+    else{
+     $(".clippy").remove();
+    }
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#4chars"),
+  handler: function(){
+     $(".clippy").remove();
+     $(".clippy-balloon").remove();
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#liveson"),
+  handler: function(direction){
+    if (direction == "down"){
+      clippy.load('Clippy', function(agent) {
+        agent.moveTo(50, 100);
+        agent.show();
+          agent.speak("Life after Office 97");
+          agent.play("SendMail");
+      });
+    }
+    else{
+     $(".clippy").remove();
+    }
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#lettermeme"),
+  handler: function(){
+     $(".clippy").remove();
+     $(".clippy-balloon").remove();
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#ebook"),
+  handler: function(direction){
+    if (direction == "down"){
+      clippy.load('Clippy', function(agent) {
+        agent.moveTo(50, 100);
+        agent.show();
+          agent.speak("Ooh la la!");
+      });
+    }
+    else{
+     $(".clippy").remove();
+    }
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#conq"),
+  handler: function(){
+     $(".clippy").remove();
+     $(".clippy-balloon").remove();
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#sexy"),
+  handler: function(direction){
+    if (direction == "down"){
+      clippy.load('Clippy', function(agent) {
+        agent.moveTo(50, 100);
+        agent.show();
+          agent.speak("I AM a sexy paperclip");
+      });
+    }
+    else{
+     $(".clippy").remove();
+    }
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#mpreg"),
+  handler: function(){
+     $(".clippy").remove();
+     $(".clippy-balloon").remove();
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#knowme"),
+  handler: function(direction){
+    if (direction == "down"){
+      clippy.load('Clippy', function(agent) {
+        agent.moveTo(50, 100);
+        agent.show();
+          agent.speak("They really like me!");
+      });
+    }
+    else{
+     $(".clippy").remove();
+    }
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#explosion"),
+  handler: function(){
+     $(".clippy").remove();
+     $(".clippy-balloon").remove();
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#bottom"),
+  handler: function(direction){
+    if (direction == "down"){
+      clippy.load('Clippy', function(agent) {
+        agent.moveTo(50, 100);
+        agent.show();
+          agent.speak("Only shooting stars...");
+          agent.play("GoodBye");
+      });
+    }
+    else{
+     $(".clippy").remove();
+    }
+  }
+});
+var waypoint = new Waypoint({
+  element: $("#sources"),
+  handler: function(){
+     $(".clippy").remove();
+     $(".clippy-balloon").remove();
+  }
+});
 // ["Congratulate", "LookRight", "SendMail", "Thinking", "Explain", "IdleRopePile", "IdleAtom", "Print", "Hide", "GetAttention", "Save", "GetTechy", "GestureUp", "Idle1_1", "Processing", "Alert", "LookUpRight", "IdleSideToSide", "GoodBye", "LookLeft", "IdleHeadScratch", "LookUpLeft", "CheckingSomething", "Hearing_1", "GetWizardy", "IdleFingerTap", "GestureLeft", "Wave", "GestureRight", "Writing", "IdleSnooze", "LookDownRight", "GetArtsy", "Show", "LookDown", "Searching", "EmptyTrash", "Greeting", "LookUp", "GestureDown", "RestPose", "IdleEyeBrowRaise", "LookDownLeft"]
 
 //play an animation
